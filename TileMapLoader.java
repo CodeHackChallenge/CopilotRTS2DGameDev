577e3524242ea703f40b2364fb122f8ea2a93ebb
CodeHackChallenge <83134756+CodeHackChallenge@users.noreply.github.com>
2025-12-08 13:55:59 -0500

    Implement TileMapLoader to load tile maps from files

---
+++ TileMapLoader.java
@@ -0,0 +1,15 @@
+class TileMapLoader {
+    public static TileMapComponent loadFromFile(String path, MapConfig config) throws IOException {
+        TileMapComponent map = new TileMapComponent(config);
+        List<String> lines = Files.readAllLines(Paths.get(path));
+
+        for (int y = 0; y < config.heightTiles() && y < lines.size(); y++) {
+            String line = lines.get(y);
+            for (int x = 0; x < config.widthTiles() && x < line.length(); x++) {
+                int id = Character.getNumericValue(line.charAt(x));
+                map.setTile(x, y, id);
+            }
+        }
+        return map;
+    }
+}
