afa886a5b756c475c74afdbc2ff9485792ae0ffa
CodeHackChallenge <83134756+CodeHackChallenge@users.noreply.github.com>
2025-12-11 12:32:28 -0500

    Update MovementSystem.java

--- MovementSystem.java
+++ MovementSystem.java
@@ -1,20 +1,32 @@
-import java.util.List;
-
 public class MovementSystem {
-    public void moveTowards(Entity e, Position target) {
+    public void moveTowards(Entity e, Position target, TileMapComponent map) {
         Position pos = e.getComponent(Position.class);
         BoundsComponent bc = e.getComponent(BoundsComponent.class);
         Sprite sprite = e.getComponent(Sprite.class);
+        if (pos == null || target == null || sprite == null) return;
 
-        if (pos != null && target != null) {
-            if (pos.x < target.x) pos.x++;
-            if (pos.x > target.x) pos.x--;
-            if (pos.y < target.y) pos.y++;
-            if (pos.y > target.y) pos.y--;
+        int nextX = pos.x;
+        int nextY = pos.y;
 
-            if (bc != null && sprite != null) {
-                bc.update(pos, sprite.width, sprite.height);
-            }
-        }
-    }
-}
+        if (pos.x < target.x) nextX++;
+        if (pos.x > target.x) nextX--;
+        if (pos.y < target.y) nextY++;
+        if (pos.y > target.y) nextY--;
+
+        int ts = map.getConfig().tileSize();
+        int leftTile   = nextX / ts;
+        int rightTile  = (nextX + sprite.width - 1) / ts;
+        int topTile    = nextY / ts;
+        int bottomTile = (nextY + sprite.height - 1) / ts;
+
+        boolean walkable =
+            map.isWalkable(leftTile, topTile) &&
+            map.isWalkable(rightTile, topTile) &&
+            map.isWalkable(leftTile, bottomTile) &&
+            map.isWalkable(rightTile, bottomTile);
+
+        if (walkable) {
+            pos.x = nextX;
+            pos.y = nextY;
+            if (bc != null) bc.update(pos, sprite.width, sprite.height);
+       
