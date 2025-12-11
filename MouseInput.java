a5028bdf5b9d29a6cda3fe1537d925b877493a35
CodeHackChallenge <83134756+CodeHackChallenge@users.noreply.github.com>
2025-12-11 10:32:43 -0500

    Update MouseInput.java

--- MouseInput.java
+++ MouseInput.java
@@ -1,38 +1,43 @@
-import java.awt.Point;
+import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.List;
 
 public class MouseInput extends MouseAdapter {
-    private List<Entity> entities;
-    private Entity selectedEntity;
+    private final List<Entity> entities;
+    private Entity selected;
     private Position moveTarget;
 
-    public MouseInput(List<Entity> entities) {
-        this.entities = entities;
-    }
+    // Hover
+    private Point mousePoint = new Point(0,0);
+
+    public MouseInput(List<Entity> entities) { this.entities = entities; }
 
     @Override
     public void mousePressed(MouseEvent e) {
-        Point click = e.getPoint();
-        boolean clickedOnEntity = false;
-
+        mousePoint = e.getPoint();
+        boolean hit = false;
         for (Entity entity : entities) {
             BoundsComponent bc = entity.getComponent(BoundsComponent.class);
-            if (bc != null && bc.bounds.contains(click)) {
-                selectedEntity = entity;
-                clickedOnEntity = true;
+            if (bc != null && bc.bounds.contains(mousePoint)) {
+                selected = entity;
+                hit = true;
                 System.out.println("Selected entity " + entity.getId());
             }
         }
-
-        if (!clickedOnEntity && selectedEntity != null) {
-            moveTarget = new Position(click.x, click.y);
+        if (!hit && selected != null) {
+            moveTarget = new Position(e.getX(), e.getY());
             System.out.println("Move command to " + moveTarget.x + "," + moveTarget.y);
         }
     }
 
-    public Entity getSelectedEntity() { return selectedEntity; }
+    @Override
+    public void mouseMoved(MouseEvent e) { mousePoint = e.getPoint(); }
+    @Override
+    public void mouseDragged(MouseEvent e) { mousePoint = e.getPoint(); }
+
+    public Entity getSelected() { return selected; }
     public Position getMoveTarget() { return moveTarget; }
     public void clearMoveTarget() { moveTarget = null; }
+    public Point getMousePoint() { return mousePoint; }
 }
