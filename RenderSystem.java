94df0b211c935b450162f4cfae668dbc371e60a6
CodeHackChallenge <83134756+CodeHackChallenge@users.noreply.github.com>
2025-12-11 12:26:45 -0500

    Update RenderSystem.java

--- RenderSystem.java
+++ RenderSystem.java
@@ -2,20 +2,20 @@ import java.awt.Graphics2D;
 import java.util.List;
 
 public class RenderSystem {
-    public void render(Graphics2D g, List<Entity> entities) {
+    public void render(Graphics2D g, List<Entity> entities, int offsetX, int offsetY) {
         for (Entity e : entities) {
             Position pos = e.getComponent(Position.class);
             Sprite sprite = e.getComponent(Sprite.class);
-            if (pos != null && sprite != null) {
-                g.setColor(sprite.color);
-                g.fillRect(pos.x, pos.y, sprite.width, sprite.height);
+            if (pos == null || sprite == null) continue;
 
-                BoundsComponent bc = e.getComponent(BoundsComponent.class);
-                if (bc != null) {
-                    g.setColor(Color.YELLOW);
-                    g.draw(bc.bounds);
-                }
-            }
+            int screenX = pos.x - offsetX;
+            int screenY = pos.y - offsetY;
+
+            if (screenX + sprite.width < 0 || screenY + sprite.height < 0 ||
+                screenX > 728 || screenY > 728) continue;
+
+            g.setColor(sprite.color);
+            g.fillRect(screenX, screenY, sprite.width, sprite.height);
         }
     }
 }
