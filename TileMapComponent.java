c956ef7615ee80dff8c12bda0ed31b9c3f488e45
CodeHackChallenge <83134756+CodeHackChallenge@users.noreply.github.com>
2025-12-11 10:05:03 -0500

    Update TileMapComponent.java
    Added isWalkable() method

--- TileMapComponent.java
+++ TileMapComponent.java
@@ -1,19 +1,19 @@
 public class TileMapComponent implements Component {
-    private int[][] tiles;
-    private MapConfig config;
+    private final int[][] tiles;
+    private final MapConfig config;
 
     public TileMapComponent(MapConfig config) {
         this.config = config;
         this.tiles = new int[config.heightTiles()][config.widthTiles()];
     }
 
-    public void setTile(int x, int y, int id) {
-        tiles[y][x] = id;
-    }
-
-    public int getTile(int x, int y) {
-        return tiles[y][x];
-    }
-
+    public void setTile(int x, int y, int id) { tiles[y][x] = id; }
+    public int getTile(int x, int y) { return tiles[y][x]; }
     public MapConfig getConfig() { return config; }
+
+    public boolean isWalkable(int tx, int ty) {
+        if (tx < 0 || ty < 0 || tx >= config.widthTiles() || ty >= config.heightTiles()) return false;
+        int id = tiles[ty][tx];
+        return id == 0 || id == 1; // 0 grass, 1 dirt are walkable
+    }
 }
