38ea214f00cf744639eedadce7bdb7342c0ea293
CodeHackChallenge <83134756+CodeHackChallenge@users.noreply.github.com>
2025-12-13 14:56:20 -0500

    Update SoldierAnimation.java

--- SoldierAnimation.java
+++ SoldierAnimation.java
@@ -1,16 +1,31 @@
+import java.awt.image.BufferedImage;
 import java.util.EnumMap;
 
 /**
- * New: SoldierAnimation holds animations for all 8 directions.
- * Flexible: can be extended for other units with fewer/more directions.
+ * Refactored SoldierAnimation:
+ * - Uses SpritesheetManager to load frames from /soldier_sheet.png
+ * - Holds animations for all 8 directions
+ * - Supports freezeStanding() and play() via Animation class
  */
 public class SoldierAnimation implements Component {
     private EnumMap<Direction, Animation> animations;
     private Direction currentDirection;
 
-    public SoldierAnimation(EnumMap<Direction, Animation> animations) {
-        this.animations = animations;
-        this.currentDirection = Direction.SOUTH; // default facing south
+    public SoldierAnimation() {
+        animations = new EnumMap<>(Direction.class);
+
+        // Build animations for all 8 directions from soldier spritesheet
+        for (int dirIndex = 0; dirIndex < 8; dirIndex++) {
+            BufferedImage[] frames = new BufferedImage[8]; // 8 frames per direction
+            for (int i = 0; i < 8; i++) {
+                frames[i] = SpritesheetManager.getInstance()
+                        .getSubImage("/soldier_sheet.png", i * 64, dirIndex * 64, 64, 64);
+            }
+            animations.put(Direction.values()[dirIndex], new Animation(frames, 8));
+        }
+
+        // Default facing south
+        currentDirection = Direction.SOUTH;
     }
 
     public void setDirection(Direction dir) {
@@ -19,5 +34,14 @@ public class SoldierAnimation implements Component {
 
     public Animation getCurrentAnimation() {
         return animations.get(currentDirection);
+    }
+
+    // Convenience methods to control animation state
+    public void freezeStanding() {
+        getCurrentAnimation().freezeStanding();
+    }
+
+    public void play() {
+        getCurrentAnimation().play();
     }
 }
