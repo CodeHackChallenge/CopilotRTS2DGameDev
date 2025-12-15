e0ad04953e22bf266e588cc21b41bad1a2d11c62
CodeHackChallenge <83134756+CodeHackChallenge@users.noreply.github.com>
2025-12-14 19:06:40 -0500

    Update Position.java

--- Position.java
+++ Position.java
@@ -1,8 +1,31 @@
-public class Position {
-    public int x, y;
-
-    public Position(int x, int y) {
-        this.x = x;
-        this.y = y;
+public class Position implements Component {
+    
+	//public int x, y; 
+	public double x;
+	public double y; 
+	
+	public Position(double x, double y) { this.x = x; this.y = y; }
+    public Position(int x, int y) { this.x = x; this.y = y; }
+    
+	public double getX() {
+		return x;
+	}
+	public void setX(double x) {
+		this.x = x;
+	}
+	public double getY() {
+		return y;
+	}
+	public void setY(double y) {
+		this.y = y;
+	}
+	
+	public int intX() {
+        return (int) Math.round(x);
     }
+
+    public int intY() {
+        return (int) Math.round(y);
+    }
+    
 }
