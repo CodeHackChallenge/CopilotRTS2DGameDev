fdd7f2dbc539d1830299e4936b24bb262db7a2f7
CodeHackChallenge <83134756+CodeHackChallenge@users.noreply.github.com>
2025-12-27 10:27:31 -0500

    Update checkthis.java

--- checkthis.java
+++ checkthis.java
@@ -1,28 +1,36 @@
-public class AnimationSystem {
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
 
-    public void update(List<Entity> entities, float delta) {
+public class EntityAnimation implements Component {
 
-        for (Entity e : entities) {
+    private Animation animation;
 
-            EntityAnimation anim = e.getComponent(EntityAnimation.class);
-            if (anim == null) continue;
+    public EntityAnimation(String spritePath, int frameCount) {
+        try {
+            BufferedImage sheet = ImageIO.read(getClass().getResourceAsStream(spritePath));
 
-            Animation a = anim.getAnimation();
-            if (a == null) continue;
+            int frameWidth = sheet.getWidth() / frameCount;
+            int frameHeight = sheet.getHeight();
 
-            // Advance animation timer
-            a.timer += delta;
+            BufferedImage[] frames = new BufferedImage[frameCount];
 
-            // If enough time passed, move to next frame
-            if (a.timer >= a.frameDelay) {
-                a.timer -= a.frameDelay;
-                a.currentFrame++;
-
-                // Loop animation
-                if (a.currentFrame >= a.totalFrames) {
-                    a.currentFrame = 0;
-                }
+            for (int i = 0; i < frameCount; i++) {
+                frames[i] = sheet.getSubimage(
+                        i * frameWidth,
+                        0,
+                        frameWidth,
+                        frameHeight
+                );
             }
+
+            animation = new Animation(frames, 0.08f); // 12.5 FPS default
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+    }
+
+    public Animation getAnimation() {
+        return animation;
     }
 }
