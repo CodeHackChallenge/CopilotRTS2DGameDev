73cb335d2783ec125c2d13e10f440f2a8dd5f3b1
CodeHackChallenge <83134756+CodeHackChallenge@users.noreply.github.com>
2025-12-28 14:04:57 -0500

    Create AttackProfile.java

---
+++ AttackProfile.java
@@ -0,0 +1,11 @@
+public class AttackProfile implements Component {
+    public float windup;
+    public float hitWindow;
+    public float recovery;
+
+    public AttackProfile(float windup, float hitWindow, float recovery) {
+        this.windup = windup;
+        this.hitWindow = hitWindow;
+        this.recovery = recovery;
+    }
+}
