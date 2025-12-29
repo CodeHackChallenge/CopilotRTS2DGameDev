087f8b80fccdf89171c230870c6ea1287d57ee1e
CodeHackChallenge <83134756+CodeHackChallenge@users.noreply.github.com>
2025-12-29 17:10:30 -0500

    Update Animation.java

--- Animation.java
+++ Animation.java
@@ -1,31 +1,18 @@
-package demo.main;
-
-import java.awt.image.BufferedImage;
- 
 public class Animation {
-    private BufferedImage[] frames;
-    private int currentFrame;
-    private int frameCount;
-    private int frameDelay;
-    private int tick; 
+    public BufferedImage[] frames;
+    public int totalFrames;
+    public int currentFrame = 0;
 
-    public Animation(BufferedImage[] frames, int frameDelay) {
+    public float frameDelay;   // seconds per frame
+    public float timer = 0f;
+
+    public Animation(BufferedImage[] frames, float frameDelay) {
         this.frames = frames;
-        this.frameCount = frames.length;
+        this.totalFrames = frames.length;
         this.frameDelay = frameDelay;
-        this.currentFrame = 0;
-        this.tick = 0;
-    }
-
-    public void update() {  
-        tick++;
-        if (tick >= frameDelay) {  
-            tick = 0; 
-            currentFrame = (currentFrame + 1) % frameCount;  
-        }
     }
 
     public BufferedImage getCurrentFrame() {
         return frames[currentFrame];
-    } 
+    }
 }
