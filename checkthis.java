b0e3f5577df361308234f151d9810ddda1508df2
CodeHackChallenge <83134756+CodeHackChallenge@users.noreply.github.com>
2025-12-31 08:24:46 -0500

    Update checkthis.java

--- checkthis.java
+++ checkthis.java
@@ -1,106 +1,62 @@
-import java.awt.Color;
-import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
-import java.util.List;
-
-public class RenderSystem {
-	 
-	
-	
-    public void render(Graphics2D g, List<Entity> entities) {
-
-        for (Entity e : entities) {
-        	RenderPosition rp = e.getComponent(RenderPosition.class);
-        	Position pos = e.getComponent(Position.class);
-        	
-            if (pos == null || rp == null) continue;
-            
-        	rp.x = (int)Math.round(pos.x);
-        	rp.y = (int)Math.round(pos.y);
-        	//System.out.println(rp.x + " "+ rp.y);
-            //round position
-           // int baseX = (int)Math.round(pos.x);
-           // int baseY = (int)Math.round(pos.y);
-            
-            // =========================================================
-            // 1. DRAW SPRITE / ANIMATION
-            // =========================================================
-            EntityAnimation animComp = e.getComponent(EntityAnimation.class);
-            if (animComp != null) {
-
-                Animation anim = animComp.getAnimation();
-                if (anim != null && anim.frames != null) {
-//                	//for smooth movement
-//                	int drawX = (int)Math.round(pos.x);
-//                	int drawY = (int)Math.round(pos.y);
-                	
-                    BufferedImage frame = anim.frames[anim.currentFrame];
-                    if (frame != null) {
-                       // g.drawImage(frame, rp.x, rp.y, null);
-                        
-                    }
-                }
-            }
-
-            // =========================================================
-            // 2. DRAW DAMAGE TEXT (always above sprite)
-            // =========================================================
-            DamageTextComponent dt = e.getComponent(DamageTextComponent.class);
-            RenderTextComponent rtc = e.getComponent(RenderTextComponent.class);
-
-            if (dt != null && rtc != null) {
-
-                g.setColor(rtc.color);
-                g.setFont(rtc.font);
-
-                int drawX = rp.x + (int)Math.round(dt.offsetX);
-                int drawY = rp.y + (int)Math.round(dt.offsetY);
-
-                g.drawString(dt.text, drawX, drawY);
-            }
-            // =========================================================
-            // Move marker
-            // =========================================================
-            MovementTarget mt = e.getComponent(MovementTarget.class);
-            if(mt != null && mt.hasTarget) {
-            	g.setColor(Color.YELLOW);
-            	g.fillOval((int)mt.x - 4, (int)mt.y - 4, 8, 8);
-            }
-            
-           
-            // =========================================================
-            // 3. DEBUG: COLLISION BOX
-            // =========================================================
-            Collision col = e.getComponent(Collision.class);
-            if (col != null) {
-//            	int colX = baseX + col.offsetX;
-//            	int colY = baseY + col.offsetY; 
-            	
-                g.setColor(new Color(0, 255, 0, 120)); // semi‑transparent green
-                g.drawRect(rp.x + col.offsetX, 
-                		   rp.y + col.offsetY, 
-                		   col.width, 
-                		   col.height);
-                
-            }
-
-            // =========================================================
-            // 4. DEBUG: ATTACK HITBOX
-            // =========================================================
-            AttackHitbox hit = e.getComponent(AttackHitbox.class);
-            if (hit != null && hit.width > 0 && hit.height > 0) {
-//            	int hitX = baseX + hit.offsetX;
-//            	int hitY = baseY + hit.offsetY; 
-            	
-                g.setColor(new Color(255, 0, 0, 120)); // semi‑transparent red
-                g.drawRect(rp.x + hit.offsetX, 
-                		   rp.y + hit.offsetY, 
-                		   hit.width, 
-                		   hit.height);
-                
-            }
-             
-        }
-    }
-     
-    
+rp.x=128 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=128 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=128 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=129 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=129 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=131 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=131 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=132 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=132 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=134 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=134 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=135 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=135 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=137 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=137 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=138 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=138 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=140 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=140 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=141 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=141 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=142 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=142 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=144 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=144 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=145 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=145 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=147 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=147 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=148 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=148 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=150 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=150 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=151 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=151 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=153 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=153 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=154 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=154 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=155 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=155 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=157 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=157 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=158 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=158 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=160 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=160 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=161 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=161 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=163 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=163 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=164 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=164 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=166 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=166 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=167 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=167 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=169 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=169 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=170 col.offsetX=16 col.width=20 bar.offsetX=-10
+rp.x=170 col.offsetX=16 col.width=20 bar.offsetX=-10
+r
